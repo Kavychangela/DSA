00b9f0b54e2e5c57b80a3a6a2a85a7b82befe1c7
Kavy Atul Changela <kavychangela89@gmail.com>
2025-10-17 14:26:06 +0530

    Refactor LinkedList implementation and update main method to use ArrayList for node creation

---
+++ Linked List/Basic/ArrtoLL.java
@@ -0,0 +1,20 @@
+import util.java.*;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int val){
+        data=val;
+        next=null;
+    }
+}
+
+public class ArrtoLL{
+    public static Node arrayToLinkedList(int[] arr) {
+        int size=arr.length;
+        if(size==0) return null;
+
+        
+    }
+}
--- Linked List/Basic/LinkedList.java
+++ Linked List/Basic/LinkedList.java
@@ -1,24 +1,60 @@
-class Node {
-    int data;
-    Node next;
+// class Node {
+//     int data;
+//     Node next;
     
-    Node(int data1, Node next1){
-        this.data=data1;
-        this.next=next1;
-    }
+//     Node(int data1, Node next1){
+//         this.data=data1;
+//         this.next=next1;
+//     }
 
-    Node(int data1){
-        this.data=data1;
-        this.next=null;
-    }
+//     Node(int data1){
+//         this.data=data1;
+//         this.next=null;
+//     }
 
-};
+// };
+
+// public class LinkedList{
+//     public static void main(String[] args) {
+//         int []arr={2,5,6,8};
+//         Node y=new Node(arr[3]);
+//         System.out.println(y.data);
+//         System.out.println(y.data);
+//     }
+// }
+
+import java.util.*;
+
+class Node{
+    public int data;
+    public Node next;
+
+    public Node(int data, Node next){
+        this.data=data;
+        this.next=next;
+    }
+}
 
 public class LinkedList{
-    public static void main(String[] args) {
-        int []arr={2,5,6,8};
-        Node y=new Node(arr[3]);
-        System.out.println(y.data);
-        System.out.println(y.data);
+    public static void main(String[] args){
+        ArrayList<Integer> arr=new ArrayList<>();
+        arr.add(2);
+        arr.add(5);
+        arr.add(8);
+        arr.add(7);
+
+        Node y1=new Node(arr.get(0), null);
+        Node y2=new Node(arr.get(1), null);
+        Node y3=new Node(arr.get(2), null);
+        Node y4=new Node(arr.get(3), null);
+
+        y1.next=y2;
+        y2.next=y3;
+        y3.next=y4;
+
+        System.out.println(y1.data+" "+ y1.next);
+        System.out.println(y2.data+" "+ y2.next);
+        System.out.println(y3.data+" "+ y3.next);
+        System.out.println(y4.data+" "+ y4.next);
     }
 }
