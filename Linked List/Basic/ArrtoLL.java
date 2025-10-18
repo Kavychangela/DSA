099e1e26407e911186ddba9d363f6708b2e79b2a
Kavy Atul Changela <kavychangela89@gmail.com>
2025-10-18 22:44:55 +0530

    Implement array to linked list conversion and add print functionality

--- Linked List/Basic/ArrtoLL.java
+++ Linked List/Basic/ArrtoLL.java
@@ -1,5 +1,3 @@
-import util.java.*;
-
 class Node{
     int data;
     Node next;
@@ -15,6 +13,32 @@ public class ArrtoLL{
         int size=arr.length;
         if(size==0) return null;
 
-        
+        Node head=new Node(arr[0]);
+        Node current=head;
+
+        for(int i=1; i<size; i++){
+            current.next=new Node(arr[i]);
+            current=current.next;
+        }
+        return head;
+    }
+
+    public static void printLinkedList(Node head){
+        Node current=head;
+        while(current!=null){
+            System.out.print(current.data + "->");
+            current=current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        // Convert array to linked list
+        Node head = arrayToLinkedList(arr);
+
+        // Print the linked list
+        printLinkedList(head);
     }
 }
